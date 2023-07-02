37627cd682a3aad7e272e36583f42d5b1d43b84b
voke <voke74ji@gmail.com>
2023-07-02 21:19:26 +0800

    save

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,10 +1,11 @@
 import Test from "../../components/LoginPage/Test";
+import StartLayout from "../../components/LoginPage/StartLayout/StartLayout";
 
 export default function index() {
   return (
     <div>
       {/* < VOKE > */}
-      <Test />
+      <StartLayout />
     </div>
   );
 }
