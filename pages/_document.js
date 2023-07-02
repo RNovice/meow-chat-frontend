1c458fb409889795fb1fc54786eb5a7ac9af6ceb
Dalufishe <fishawe87@gmail.com>
2023-07-02 16:06:40 +0800

    feat: api-test

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,10 @@
-import { cx } from "@emotion/css";
+import { cx, css } from "@emotion/css";
 import Document, { Html, Head, NextScript, Main } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html
-        lang="en"
-        className={cx(css`
-          overflow-y: scroll;
-          overscroll-behavior-y: none;
-        `)}
-      >
+      <Html lang="en">
         <Head></Head>
         <body>
           <Main></Main>
--- pages/home/index.js
+++ pages/home/index.js
@@ -2,12 +2,25 @@ import { cx } from "@emotion/css";
 import Navbar from "../../components/global/Navbar/Navbar";
 import Tabbar from "../../components/global/Tabbar/Tabbar";
 import Searchbar from "../../components/HomePage/Searchbar/Searchbar";
+import { useEffect, useState } from "react";
 
 export default function index() {
+
+  const [test, setTest] = useState({});
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:1314/api/helloworld")
+      .then((res) => res.json())
+      .then((json) => {
+        setTest(json);
+      });
+  });
+
   return (
     <div className={cx("w-screen h-screen", "bg-m1")}>
       <Navbar />
       <Searchbar />
+      <div className="text-white">{test?.hello}</div>
       <div className={cx("fixed bottom-0")}>
         <Tabbar />
       </div>
