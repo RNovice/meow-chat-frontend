27dc055ecc40b8c1c5b7fcfdf93c005ac6dd8f11
Voke <qaqa456zh@gmail.com>
2023-07-07 15:03:32 +0800

    login page layout

--- components/LoginPage/StartLayout/StartLayout.jsx
+++ components/LoginPage/StartLayout/StartLayout.jsx
@@ -4,17 +4,22 @@ import { css, cx } from "@emotion/css"
 // import testData from "../../dummy-data/test.json"
 import png from "../images/icon.png";
 
-export default function StartLayout() {
+export default function StartLayout(props) {
 
     console.log(png.src)
 
     return (
 
-        <div className={cx("bg-m1", "w-screen h-screen", "flex flex-col items-center" )}>
-          <img className={cx()} src={png.src} alt="Logo" />
-          <button className={cx("min-w-[200px]", "bg-m3 ", "rounded", "p-2", "text-m1 text-sm tracking-[4px]")}>
-            Start
-          </button>
+        <div className={cx("bg-m1", "w-screen h-screen", "flex items-center justify-center")}>
+          <div className={cx("flex flex-col items-center" )}>
+            <img className={cx("h-[250px]")} src={png.src} alt="Logo" />
+            <button 
+              className={cx("min-w-[200px]", "bg-m3 ", "rounded", "p-2", "text-m1 text-sm font-extrabold tracking-[4px]")}
+              onClick={() => props.setLayout('login')}
+            >
+              Start
+            </button>
+          </div>
         </div>
     )
 }
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,21 @@
+import React, {useState} from 'react'
+import { css, cx } from "@emotion/css"
 import Test from "../../components/LoginPage/Test";
 import StartLayout from "../../components/LoginPage/StartLayout/StartLayout";
+import LoginLayout from "../../components/LoginPage/LoginLayout/LoginLayout";
+import SignUpLayout from "../../components/LoginPage/SignUpLayout/SignUpLayout";
 
 export default function index() {
+  const [layout, setLayout] = useState("start")
   return (
     <div>
-      {/* < VOKE > */}
-      <StartLayout />
+      {
+        layout === "signup" ?
+          <SignUpLayout setLayout={setLayout}/> :
+        layout === "login" ? 
+          <LoginLayout setLayout={setLayout}/> :
+          <StartLayout setLayout={setLayout}/>
+      }
     </div>
   );
 }
